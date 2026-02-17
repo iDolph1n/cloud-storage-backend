8be2436e73c4d5d8c8ca1bb83a5365ff10f38bf7
Vitaliy Mikhaylov <vitaly.mikhaylov94@yandex.ru>
2026-02-18 01:29:05 +0500

    Init project: Spring Boot skeleton, pom.xml, .gitignore

---
+++ src/main/java/ru/netology/cloudstorage/CloudStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.cloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class CloudStorageApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CloudStorageApplication.class, args);
+  }
+}
